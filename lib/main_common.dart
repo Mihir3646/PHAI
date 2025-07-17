2aaaa0a6ba85b510a242b26c0c8706914c984748
Mihir Trivedi <Mihir3646@users.noreply.github.com>
2025-07-17 20:35:16 +0530

    Initialize PythonFfi before running app

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:python_ffi/python_ffi.dart';
 import 'app.dart';
 
 Future<void> mainCommon(String envFile) async {
   WidgetsFlutterBinding.ensureInitialized();
+  await PythonFfi.instance.initialize();
   await dotenv.load(fileName: envFile);
   await GetStorage.init();
   runApp(const App());
